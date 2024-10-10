ea4ec096e8b49a99c8aea634e3f0aae4eb16af6b
ChrisZuo <39025643+Dr-Left@users.noreply.github.com>
2024-10-10 19:44:18 +0800

    Code formatting

--- shelfer.py
+++ shelfer.py
@@ -1,29 +1,32 @@
-''' shelfer.py - Store the vocabulary information into the shelf.
+""" shelfer.py - Store the vocabulary information into the shelf.
 
     typical usage: shelfer.work(basisName)
-'''
-import xlrd
-import shelve
+"""
+
 import os
+import re
+import shelve
 import sys
 import time
-import re
+
+import xlrd
+
 
 def work(fname):
-    '''
+    """
     fname is the Excel word list basis
-    '''
+    """
     if fname == "":
         # use the last shelf
-        assert os.path.exists('.\\data\\dict.dat')
+        assert os.path.exists(".\\data\\dict.dat")
         return 0
     else:
         # create new shelf, with the basis in fname
-        if os.path.exists('.\\data\\dict.dat'):
-            os.remove('.\\data\\dict.dat')
-            os.remove('.\\data\\dict.bak')
-            os.remove('.\\data\\dict.dir')
-        with shelve.open('.\\data\dict') as shelfFile:
+        if os.path.exists(".\\data\\dict.dat"):
+            os.remove(".\\data\\dict.dat")
+            os.remove(".\\data\\dict.bak")
+            os.remove(".\\data\\dict.dir")
+        with shelve.open(".\\data\dict") as shelfFile:
             wb = xlrd.open_workbook(fname)
             ws = wb.sheet_by_index(0)
             # get the available row and column numbers
@@ -32,11 +35,11 @@ def work(fname):
             wordCol, transCol = whereAreWordsAndTranslations(ws)
             for i in range(1, rowNum):
                 word = str(ws.cell_value(i, wordCol))
-                word = ''.join(word.split())
+                word = "".join(word.split())
                 translation = str(ws.cell_value(i, transCol))
-                translation = ''.join(translation.split())
+                translation = "".join(translation.split())
                 shelfFile[word] = translation
-            print('词库数据创建成功！')
+            print("词库数据创建成功！")
             shelfFile.close()
 
 
@@ -44,37 +47,38 @@ def whereAreWordsAndTranslations(ws):
     # find where the English words are
     rowNum = ws.nrows
     colNum = ws.ncols
-    wordRegex = re.compile(r'^[a-zA-Z]+$')  # English words
+    wordRegex = re.compile(r"^[a-zA-Z]+$")  # English words
     wordCol = -1
     for i in range(colNum):
         for j in range(1, rowNum):
             val = str(ws.col_values(i)[j])
-            val = ''.join(val.split())
+            val = "".join(val.split())
             if wordRegex.search(val):
                 wordCol = i
                 break
         if wordCol != -1:
             break
     if wordCol == -1:
-        raise Exception('Words no find.')
+        raise Exception("Words no find.")
     # find where the translations are
-    chineseRegex = re.compile(r'[\u4e00-\u9fa5]+')  # Chinese characters
+    chineseRegex = re.compile(r"[\u4e00-\u9fa5]+")  # Chinese characters
     transCol = -1
     for i in range(colNum):
         if i == wordCol:
             continue
         for j in range(1, rowNum):
             val = str(ws.col_values(i)[j])
-            val = ''.join(val.split())
+            val = "".join(val.split())
             if chineseRegex.search(val):
                 transCol = i
-                #print('DEBUG:' + val)
+                # print('DEBUG:' + val)
                 break
         if transCol != -1:
             break
     if transCol == -1:
-        raise Exception('Translations no find.')
+        raise Exception("Translations no find.")
     return wordCol, transCol
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     work(sys.argv[1])
--- wordExtract.py
+++ wordExtract.py
@@ -1,4 +1,4 @@
-''' wordExtract.py - Extract words from texts in bin and return a dictionary
+""" wordExtract.py - Extract words from texts in bin and return a dictionary
 with keys of words and values of the times the word appears.
 
 p.s. Compatible with documents ends with .txt, .docx
@@ -10,124 +10,130 @@ wordList = wordExtract.extract()
 
 arguments: <openPath1> <savePath> [basisPath]
 
-'''
+"""
 
-import os
-import glob
-import re
-import docx
-import time
-import logging
-import sys
-import openpyxl
-import currentTime
 import codecs
+import glob
+import logging
+import os
+import re
 import shelve
-import shelfer # my own module shelfer.py
+import sys
+import time
+
+import docx
+import openpyxl
 from openpyxl.styles import Font
+
+import currentTime
+import shelfer  # my own module shelfer.py
 
 basisPath = ""
 
+
 def output(fname, wordList):
     # generate the shelf of the dictionary in .\data
-    shelfer.work(basisPath) # if the GUI not send it, it remains blank
-    #create an excel file
-    wb = openpyxl.Workbook()    # workbook
-    ws = wb.active   # worksheet
+    shelfer.work(basisPath)  # if the GUI not send it, it remains blank
+    # create an excel file
+    wb = openpyxl.Workbook()  # workbook
+    ws = wb.active  # worksheet
     # initial the headline
-    ws['A1'] = '序号'
-    ws['B1'] = '单词'
-    ws['C1'] = '释意'
-    ws['D1'] = '词频'
+    ws["A1"] = "序号"
+    ws["B1"] = "单词"
+    ws["C1"] = "释意"
+    ws["D1"] = "词频"
     # output the words and the numbers
     line = 1
-    shelfFile = shelve.open('.\\data\dict')
+    shelfFile = shelve.open(".\\data\dict")
     if shelfFile.keys() == []:
-        raise Exception('Run shelfer.py first!')
+        raise Exception("Run shelfer.py first!")
     for word, num in wordList.items():
         try:
-            #Another mode:get from the internet: translation = trans.getTranslation(word)
+            # Another mode:get from the internet: translation = trans.getTranslation(word)
             if word not in shelfFile.keys():
                 continue
             translation = shelfFile[word]
             line += 1
-            ws['A' + str(line)] = line - 1
-            ws['B' + str(line)] = word
-            ws['C' + str(line)] = translation
-            ws['D' + str(line)] = num
+            ws["A" + str(line)] = line - 1
+            ws["B" + str(line)] = word
+            ws["C" + str(line)] = translation
+            ws["D" + str(line)] = num
         except Exception as exc:
-            print('Translation error: %s' %(exc))
-            
+            print("Translation error: %s" % (exc))
+
     # set the column width
-    ws.column_dimensions['A'].width = 5
-    ws.column_dimensions['B'].width = 17
-    ws.column_dimensions['C'].width = 90
-    ws.column_dimensions['D'].width = 7
+    ws.column_dimensions["A"].width = 5
+    ws.column_dimensions["B"].width = 17
+    ws.column_dimensions["C"].width = 90
+    ws.column_dimensions["D"].width = 7
     # set the font
     myFont = Font(name="Times New Roman", size=11)
     for i in range(len(wordList) + 1):
         for j in range(4):
-            ws.cell(i + 1, j + 1).font = myFont # cell 是从1开始
-    #save
+            ws.cell(i + 1, j + 1).font = myFont  # cell 是从1开始
+    # save
     wb.save(fname)
     print(os.path.abspath(fname))
     print(line - 1)
     return 0
 
+
 def cutUp(text):
     text = text.lower()
     # not an English alphabet
-    myRegex = re.compile(r'[^a-zA-Z]+')# this should be plus but not star
-    text = myRegex.sub(' ', text)
-    
-    #print(myRegex.findall(text))
-    #print('\n\n\n\n' + text)
-    
+    myRegex = re.compile(r"[^a-zA-Z]+")  # this should be plus but not star
+    text = myRegex.sub(" ", text)
+
+    # print(myRegex.findall(text))
+    # print('\n\n\n\n' + text)
+
     return text
+
 
 def extract(openPath, savePath):
     currentDirectory = os.getcwd()
     words = {}
-    #print(openPath)
+    # print(openPath)
     # for each text, collect the words and the corresponding appearing times
     dr = openPath
-    if (dr.endswith(".txt")):
+    if dr.endswith(".txt"):
         try:
             myFile = open(dr)
         except Exception as exc:
-            print('cannot open directory.')
+            print("cannot open directory.")
         try:
             cont = myFile.read()
         except Exception as exc:
-            myFile = codecs.open(dr, mode='r', encoding='utf-8')
+            myFile = codecs.open(dr, mode="r", encoding="utf-8")
             cont = myFile.read()
         cont = cutUp(cont)
         wordList = cont.split()
         for wd in wordList:
             words.setdefault(wd, 0)
             words[wd] += 1
-    elif (dr.endswith(".doc") or dr.endswith(".docx")):
+    elif dr.endswith(".doc") or dr.endswith(".docx"):
         try:
             myFile = docx.Document(dr)
         except Exception as exc:
-            print('cannot open directory.')
-        cont = ''
+            print("cannot open directory.")
+        cont = ""
         for paragraph in myFile.paragraphs:
             cont += paragraph.text
         cont = cutUp(cont)
         wordList = cont.split()
         for wd in wordList:
             words.setdefault(wd, 0)
-            words[wd] += 1    
-   
-    #words = sorted(words.items())
-    #print('词语提取成功')
-    #print(words)
-    #time.sleep(0.5)
-    #print(words)
-    return words   #    return a dictionary of words   
+            words[wd] += 1
 
-if __name__=='__main__':
+    # words = sorted(words.items())
+    # print('词语提取成功')
+    # print(words)
+    # time.sleep(0.5)
+    # print(words)
+    return words  #    return a dictionary of words
+
+
+if __name__ == "__main__":
     if len(sys.argv) < 3:
         sys.exit(-1)
     openPath = sys.argv[1]
@@ -136,11 +142,3 @@ if __name__=='__main__':
         basisPath = sys.argv[3]
     words = extract(openPath, savePath)
     output(savePath, words)
-
-
-
-
-
-
-
-
